9875282112af4e8460fb7fe13a51d031102f74f3
nattensleplubo <nathan.gobert2002@gmail.com>
2022-10-23 12:11:05 +0200

    C00 ex01 done

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/22 13:01:22 by ngobert           #+#    #+#             */
+/*   Updated: 2022/10/22 13:18:27 by ngobert          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+// #include <cstring>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int j = 1; j < argc; j++)
+		{
+			for (size_t i = 0; i < std::strlen(argv[j]); i++)
+				std::putchar(std::toupper(argv[j][i]));
+			std::cout << " ";
+		}
+		std::cout << std::endl;
+	}
+}
