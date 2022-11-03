d0e2d568e430b746de8aa11faf78ed11357e48f3
nattensleplubo <nathan.gobert2002@gmail.com>
2022-11-03 18:02:36 +0100

    merde covide chiasse cul

--- cpp00/ex01/Contact.hpp
+++ cpp00/ex01/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 16:29:49 by ngobert           #+#    #+#             */
-/*   Updated: 2022/11/03 14:26:43 by ngobert          ###   ########.fr       */
+/*   Updated: 2022/11/03 16:43:40 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,19 @@
 
 # define CONTACT_HPP
 
+# include <iostream>
+
 class Contact
 {
 	private:
-		/* args */
+		std::string	_first_name_;
+		std::string	_last_name_;
+		std::string	_nickname_;
+		std::string	_phone_number_;
+		std::string	_darkest_secret_;
 	public:
+		int	set_contact(std::string _first_name_, std::string _last_name_,
+			std::string _nickname_, std::string _phone_number_, std::string _darkest_secret_);
 		Contact(/* args */);
 		~Contact();
 };
--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 16:15:18 by ngobert           #+#    #+#             */
-/*   Updated: 2022/11/03 15:19:43 by ngobert          ###   ########.fr       */
+/*   Updated: 2022/11/03 16:47:33 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,24 @@
 
 void	do_add(PhoneBook *phonebook)
 {
-	std::string	
+	std::string	input[5];
+
+	std::cout << "First name : ";
+	std::getline(std::cin, input[0]);
+	std::cout << "Last name : ";
+	std::getline(std::cin, input[1]);
+	std::cout << "Nickname : ";
+	std::getline(std::cin, input[2]);
+	std::cout << "Phone number : ";
+	std::getline(std::cin, input[3]);
+	std::cout << "Darkest Secret : ";
+	std::getline(std::cin, input[4]);
 }
 
 void	do_command(std::string input, PhoneBook *phonebook)
 {
 	if (input == "ADD")
-		std::cout << "ADD" << std::endl;
+		do_add(phonebook);
 	else if (input == "SEARCH")
 		std::cout << "SEARCH" << std::endl;
 	else if (input == "EXIT")
