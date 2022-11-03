792b5e4d00065433c01e201c944d92e8684b9191
nattensleplubo <nathan.gobert2002@gmail.com>
2022-11-03 13:57:23 +0100

    J'<3 WHITEARMOR

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -6,11 +6,23 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 16:15:18 by ngobert           #+#    #+#             */
-/*   Updated: 2022/10/28 16:49:44 by ngobert          ###   ########.fr       */
+/*   Updated: 2022/10/28 19:42:53 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes.h"
+
+void	do_command(std::string input, PhoneBook *phonebook)
+{
+	if (input == "ADD")
+		std::cout << "ADD" << std::endl;
+	else if (input == "SEARCH")
+		std::cout << "SEARCH" << std::endl;
+	else if (input == "EXIT")
+		exit (0);
+	else
+		std::cout << "Command not found." << std::endl;
+}
 
 int	main(void)
 {
@@ -21,7 +33,6 @@ int	main(void)
 	{
 		std::cout << RED << "Phonebook >> " << CRESET;
 		std::getline(std::cin, input);
-		std::cout << input << std::endl;
+		do_command(input, &phonebook);
 	}
-	
 }
