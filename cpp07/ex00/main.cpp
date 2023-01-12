b090e412f0c649f2ab88ffcf24ad130656e4fba1
nattensleplubo <nathan.gobert2002@gmail.com>
2023-01-12 18:57:41 +0100

    07.01 done

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 18:38:09 by ngobert           #+#    #+#             */
-/*   Updated: 2023/01/12 18:50:21 by ngobert          ###   ########.fr       */
+/*   Updated: 2023/01/12 18:57:04 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,24 +20,26 @@ int main( void )
 	int a = 2;
 	int b = 3;
 	
-	std::cout << "Before swap :" << std::endl;
-	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << std::endl << "Before swap :" << std::endl;
+	std::cout << "a = " << a << ", b = " << b << std::endl << std::endl;
 	::swap( a, b );
 	std::cout << "After swap :" << std::endl;
-	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "a = " << a << ", b = " << b << std::endl << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl << std::endl;
+
+	std::cout << "/*************************/" << std::endl << std::endl;
 	
 	std::string c = "chaine1";
 	std::string d = "chaine2";
 	
 	std::cout << "Before swap :" << std::endl;
-	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "c = " << c << ", d = " << d << std::endl << std::endl;
 	::swap(c, d);
 	std::cout << "After swap :" << std::endl;
-	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "c = " << c << ", d = " << d << std::endl << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl << std::endl;
 	
 	return 0; 
 }
